b1557169159040b89d51ba0d6cea782a9ba9a697
Chia-Wei Li <dreamcwli@gmail.com>
2012-11-17 02:47:55 +0800

    Add my_wingray build

---
+++ my_wingray.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/moto/wingray/full_wingray.mk)
+
+PRODUCT_NAME := my_wingray
+PRODUCT_DEVICE := wingray
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Xoom
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=tervigon \
+    PRIVATE_BUILD_DESC="tervigon-user 4.1.2 JZO54K 485486 release-keys" \
+    BUILD_FINGERPRINT="motorola/tervigon/wingray:4.1.2/JZO54K/485486:user/release-keys"
