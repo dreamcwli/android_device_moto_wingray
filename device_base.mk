7283aef90a9d7c7f135b134b93d4dfa197acfa8e
Chia-Wei Li <dreamcwli@gmail.com>
2013-01-07 19:25:00 +0800

    Fix inheritance for stingray and umts_everest builds
    Change-Id: I079b88ac35b6d411e6ce479f838407d67b85630e

--- device_base.mk
+++ device_base.mk
@@ -113,9 +113,7 @@ PRODUCT_COPY_FILES += \
     system/extras/bugmailer/send_bug:system/bin/send_bug
 
 # inherit from the non-open-source side, if present
-ifneq ($(filter trygon trygon_l10n calgon full_stingray tyranid stingray my_stingray,$(TARGET_PRODUCT)),)
-$(call inherit-product-if-exists, vendor/moto/stingray/stingray-vendor.mk)
-else
+ifneq ($(filter full_wingray my_wingray,$(TARGET_PRODUCT)),)
 $(call inherit-product-if-exists, vendor/moto/wingray/wingray-vendor.mk)
 endif
 
